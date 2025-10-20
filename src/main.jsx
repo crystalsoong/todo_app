0b27b15009b92c9972b4379868d90f232ba5d32e
Crystal Soong cs785 <crystal.soong@duke.edu>
2025-10-19 23:23:53 -0400

    updated main with todo

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,24 @@
 import { createRoot } from 'react-dom/client';
+import { useState } from "react";
+import WelcomePage from './WelcomePage';
+import ToDo from './TodoPage'
+
+function App() {
+  const [name, setName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  return (
+    <>
+      {!submitted ? (
+        <WelcomePage
+          name={name}
+          setName={setName}
+          onSubmit={() => setSubmitted(true)}
+        />
+      ) : (<ToDo name = {name}/>)}
+    </>
+  );
+}
+export default App;
 
 createRoot(document.getElementById('root')).render(
     <App />
